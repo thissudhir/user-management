d344074b7a7e681b322b4225c3b9264fcc1e0a47
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2024-09-30 19:25:46 +0530

    second commit

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -9,58 +9,50 @@ import {
   TableRow,
   Paper,
   TextField,
+  Autocomplete,
   Box,
-  InputAdornment,
 } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
 
-const UserList = () => {
+function UserList() {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    //NOTE:CHECKING THE LOCALSTORAGE
     const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
     setUsers(storedUsers);
   }, []);
-  //NOTE:
 
   const filteredUsers = users.filter((user) =>
-    Object.values(user).some((value) =>
-      value.toString().toLowerCase().includes(searchTerm.toLowerCase())
-    )
+    user.email.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-      <TextField
-        fullWidth
-        variant="outlined"
-        label="Search users"
-        value={searchTerm}
-        onChange={handleSearchChange}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
+      <Autocomplete
+        options={users}
+        getOptionLabel={(option) => option.email}
+        renderInput={(params) => (
+          <TextField {...params} label="Search by email" />
+        )}
+        onInputChange={(event, newInputValue) => {
+          setSearchTerm(newInputValue);
         }}
+        renderOption={(props, option) => (
+          <li {...props}>
+            {option.firstName} {option.lastName} ({option.email})
+          </li>
+        )}
       />
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>NAME</TableCell>
-              <TableCell>PHONE NO.</TableCell>
-              <TableCell>TYPE</TableCell>
-              <TableCell>FUNCTION</TableCell>
-              <TableCell>LOCATION</TableCell>
-              <TableCell>ACTION</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Phone</TableCell>
+              <TableCell>Role</TableCell>
+              <TableCell>Department</TableCell>
+              <TableCell>Location</TableCell>
+              <TableCell>Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -81,6 +73,6 @@ const UserList = () => {
       </TableContainer>
     </Box>
   );
-};
+}
 
 export default UserList;
